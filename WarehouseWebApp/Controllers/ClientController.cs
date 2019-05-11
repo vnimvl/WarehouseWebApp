87470a87d832fa0adb1bf68dd4fe2ab613bc42ee
vnimvl <rsobczak96@gmail.com>
2019-05-11 23:25:29 +0200

    dodanie przykłądowego szablonu dodawania nowego klienta

---
+++ WarehouseWebApp/Controllers/ClientController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WarehouseWebApp.Controllers
+{
+    [Authorize]
+    public class ClientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult NewClient()
+        {
+            return View();
+        }
+    }
+}

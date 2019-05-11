de675bce5a1bec40bd4d403c8298e23877e5ce7d
vnimvl <rsobczak96@gmail.com>
2019-05-11 14:30:13 +0200

    zaktualizowanie bazy o model przechowywania uzytkownikow Identity oraz dodanie tabeli produkty

--- WarehouseWebApp/Data/Migrations/20190511122817_identity_oraz_model_produktu.Designer.cs
+++ WarehouseWebApp/Data/Migrations/20190511122817_identity_oraz_model_produktu.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WarehouseWebApp.Data;
 
 namespace WarehouseWebApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190511122817_identity_oraz_model_produktu")]
+    partial class identity_oraz_model_produktu
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WarehouseWebApp/Data/Migrations/20190511122817_identity_oraz_model_produktu.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WarehouseWebApp.Data.Migrations
+{
+    public partial class identity_oraz_model_produktu : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produkty",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    SerialNumber = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Count = table.Column<int>(nullable: false),
+                    Price = table.Column<float>(nullable: false),
+                    VAT = table.Column<int>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    IsActive = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produkty", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produkty");
+        }
+    }
+}

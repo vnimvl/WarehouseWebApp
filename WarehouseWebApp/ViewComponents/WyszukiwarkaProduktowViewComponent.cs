7294092480c776ffe73817301b051598e8752291
vnimvl <rsobczak96@gmail.com>
2019-05-12 18:20:25 +0200

    naprawa wyswietlania wyszukiwarki po serialu

--- WarehouseWebApp/ViewComponents/WyszukiwarkaProduktowViewComponent.cs
+++ WarehouseWebApp/ViewComponents/WyszukiwarkaProduktowViewComponent.cs
@@ -18,9 +18,9 @@ namespace WarehouseWebApp.ViewComponents
         public IViewComponentResult Invoke(int serial, DateTime dataStart, DateTime dataKoniec)
         {
             
+            var konkretneProdukty = _produktyCtx.Produkty.Where(p => p.SerialNumber == serial).ToList();
             if (serial > 0 || dataStart == null || dataKoniec == null)
             {
-                var konkretneProdukty = _produktyCtx.Produkty.Where(p => p.SerialNumber == serial).ToList();
                 return View("Default", konkretneProdukty);
             }else
             if(serial == 0 || dataStart != null || dataKoniec != null)
@@ -28,8 +28,8 @@ namespace WarehouseWebApp.ViewComponents
                 var produktyZDanegoDnia = _produktyCtx.Produkty.Where(p => p.ShipmenDate >= dataStart && p.ShipmenDate <= dataKoniec.AddDays(1)).ToList();
                 return View("WybranyDzien", produktyZDanegoDnia);
             }
-             var pusta = _produktyCtx.Produkty.Where(p => p.SerialNumber == 0).ToList();
-             return View("Default", pusta);
+ 
+             return View("Default", konkretneProdukty);
         }
     }
 }
